8b54728b2d4592f1be5d051d8075accfbd5046af
Guz <darckProyect8@gmail.com>
2023-08-08 14:24:37 -0500

    Footer

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -3,25 +3,30 @@ import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <nav className="p-4 flex">
-      <ul className="flex gap-2 w-[90rem] m-auto">
-        {routes.map(({ label, path }) => (
-          <li key={path}>
-            <NavLink
-              className={({ isActive, isPending }) =>
-                isActive
-                  ? "font-bold scale-105"
-                  : isPending
-                  ? "underline"
-                  : "text-gray-400"
-              }
-              to={path}
-            >
-              {label.toUpperCase()}
-            </NavLink>
-          </li>
-        ))}
-      </ul>
+    <nav className="px-8 py-4 bg-slate-800 flex items-center justify-between">
+      <div>
+        <img src="vite.svg" alt="logo" width={20} height={20} />
+      </div>
+      <div>
+        <ul className="flex gap-2 m-auto">
+          {routes.map(({ label, path }) => (
+            <li key={path}>
+              <NavLink
+                className={({ isActive, isPending }) =>
+                  isActive
+                    ? "font-bold scale-105 text-white"
+                    : isPending
+                    ? "underline"
+                    : "text-gray-400"
+                }
+                to={path}
+              >
+                {label.toUpperCase()}
+              </NavLink>
+            </li>
+          ))}
+        </ul>
+      </div>
     </nav>
   );
 };
---
+++ src/helpers/data.ts
@@ -0,0 +1,68 @@
+
+// export const linksBelow = [
+//   {
+//     title: "Product",
+//     links: [
+//       {
+//         label: "Updates",
+//         path: "/",
+//       },
+//       {
+//         label: "Downloads",
+//         path: "/",
+//       },
+//       {
+//         label: "Beta test",
+//         path: "/",
+//       },
+//       {
+//         label: "Pricing products",
+//         path: "/",
+//       },
+//     ],
+//   },
+//   {
+//     title: "Support",
+//     links: [
+//       {
+//         label: "Help center",
+//         path: "/",
+//       },
+//       {
+//         label: "Account Information",
+//         path: "/",
+//       },
+//       {
+//         label: "About",
+//         path: "/",
+//       },
+//       {
+//         label: "Contact us",
+//         path: "/",
+//       },
+//     ],
+//   },
+//   {
+//     title: "Help and Solutions",
+//     links: [
+//       {
+//         label: "Talk to support",
+//         path: "/",
+//       },
+//       {
+//         label: "Support docs",
+//         path: "/",
+//       },
+//       {
+//         label: "System status",
+//         path: "/",
+//       },
+//       {
+//         label: "Response",
+//         path: "/",
+//       },
+//     ],
+//   },
+// ];
+
+
